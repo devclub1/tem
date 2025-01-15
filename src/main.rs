709ba8c364a5d3c92bde0ecb563c3799c2235812
axbg <bisagalexstefan@gmail.com>
2025-01-15 21:46:45 +0200

    Add clap interface for available commands

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,12 @@ mod gitprocessor;
 mod loader;
 mod processor;
 
-use clap::Parser;
+use clap::Arg;
+use clap::Command;
+use clap::{ArgMatches, Parser};
 use processor::Processor;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::panic;
 use std::path::Path;
@@ -22,27 +25,73 @@ fn main() {
     bind_panic_handler();
     create_config_file(&config::build_default_home_config_path());
 
-    let args = Args::parse();
     let processors = loader::load_processors();
-    let configs = load_config();
 
-    match execute_processor(processors, configs, args) {
-        true => println!("\nhappy coding! (◕‿◕)"),
-        false => println!("\nouch, something bad happened during processing :("),
-    }
+    let matches = build_commands_interface(processors);
+
+    // let args = Args::parse();
+    // let configs = load_config();
+    //
+    // match execute_processor(processors, configs, args) {
+    //     true => println!("\nhappy coding! (◕‿◕)"),
+    //     false => println!("\nouch, something bad happened during processing :("),
+    // }
 }
 
 fn bind_panic_handler() {
     panic::set_hook(Box::new(|info| {
         if let Some(message) = info.payload().downcast_ref::<String>() {
             if message.starts_with("show:") {
-                println!("{}", &message[5..]);
+                println!("{}", &message.strip_prefix("show:").unwrap());
                 return;
             }
         }
 
         println!("{}", info);
     }));
+}
+
+fn build_commands_interface(processors: HashMap<String, Box<dyn Processor>>) -> ArgMatches {
+    let processors_set: HashSet<String> = processors
+        .values()
+        .map(|obj| obj.as_ref().types().to_string())
+        .collect();
+
+    let valid_processors = processors_set
+        .iter()
+        .cloned()
+        .collect::<Vec<String>>()
+        .join(", ");
+
+    Command::new("tem")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about(env!("CARGO_PKG_DESCRIPTION"))
+        .subcommand(Command::new("init").about("Initializes tem config file"))
+        .subcommand(Command::new("config").about("Lists the current config file"))
+        .subcommand(
+            Command::new("proc")
+                .about("Prints all available processors or information about a specific processor")
+                .arg(
+                    Arg::new("name")
+                        .value_name("Processor name")
+                        .help(format!(
+                            "The 'name' argument must be one of the following valid processors: {}",
+                            &valid_processors
+                        ))
+                        .value_parser(move |val: &str| {
+                            if processors_set.contains(val) {
+                                Ok(val.to_string())
+                            } else {
+                                Err(format!(
+                                    "Invalid processor name '{}'. Valid values are: {}",
+                                    val, &valid_processors
+                                ))
+                            }
+                        })
+                        .required(false),
+                ),
+        )
+        .get_matches()
 }
 
 fn create_config_file(config_path: &str) {
@@ -76,12 +125,9 @@ fn execute_processor(
     for (section, value) in &configs {
         if let Value::Table(table) = value {
             if table.contains_key(&args.template) && processors.contains_key(section) {
-                match processors.get(section).take() {
-                    Some(processor) => {
-                        let config: &Value = table.get(&args.template).unwrap();
-                        return processor.process(args, config);
-                    }
-                    None => {}
+                if let Some(processor) = processors.get(section).take() {
+                    let config: &Value = table.get(&args.template).unwrap();
+                    return processor.process(args, config);
                 }
             }
         }
